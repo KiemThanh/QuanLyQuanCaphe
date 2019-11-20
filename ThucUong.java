2062552db14fdc50fa67b249706c17a5ac604c7e
KiemThanh <thanhcius@gmail.com>
2019-11-20 13:55:24 +0700

    model ThucUong

---
+++ ThucUong.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanliquancafe.model;
+
+/**
+ *
+ * @author admin
+ */
+public class ThucUong extends Mon {
+    public ThucUong(){
+        
+    }
+    
+    public ThucUong(String maM, String ten, double gt, boolean tt){
+        super(maM, ten, gt, tt);
+        
+    }
+}
