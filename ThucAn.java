646e3df598549a1915d0a1c33e7805fad89d61e3
KiemThanh <thanhcius@gmail.com>
2019-11-20 13:54:47 +0700

    model Thuc An

---
+++ ThucAn.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanliquancafe.model;
+
+/**
+ *
+ * @author admin
+ */
+public class ThucAn extends Mon {
+    
+    public ThucAn(){
+        
+    }
+    
+    public ThucAn(String maM, String ten, double gt, boolean tt){
+        super(maM, ten, gt, tt);
+        
+    }
+    
+}
